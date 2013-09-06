2a4518d28958a45fcd42e46968e4c7e128e20893
William B. Wettersten <wbw20@case.edu>
2013-09-06 17:02:03 -0400

    stub out post routes

--- src/server.js
+++ src/server.js
@@ -10,10 +10,20 @@ app.get('/buy', function(req, res) {
   });
 });
 
+app.post('/buy', function(req, res) {
+  console.log(req.body);
+  res.send(200);
+});
+
 app.get('/sell', function(req, res) {
   res.send({
     "message": 'going to be a list of sell orders'
   });
+});
+
+app.post('/sell', function(req, res) {
+  console.log(req.body);
+  res.send(200);
 });
 
 app.listen(8080);
