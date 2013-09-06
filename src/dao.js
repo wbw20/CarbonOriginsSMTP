5542edb1bad19f5e990c9ccce7e9158aff4513da
William B. Wettersten <wbw20@case.edu>
2013-09-06 17:15:39 -0400

    stub dao functions

--- src/dao.js
+++ src/dao.js
@@ -1,1 +1,17 @@
-dao.js
+var sqlite3 = require('sqlite3');
+var db = new sqlite3.Database(':memory:');
+
+module.exports = Object.freeze({
+  setup: function() {
+
+  },
+  seed: function() {
+
+  },
+  buys: function(options) {
+
+  },
+  sells: function(options) {
+
+  }
+});
