28c87a9922e5c74fb858c1c5c92067853f10c2c9
William B. Wettersten <wbw20@case.edu>
2013-09-06 21:19:54 -0400

    create a basic db schema

--- src/dao.js
+++ src/dao.js
@@ -3,7 +3,14 @@ var db = new sqlite3.Database(':memory:');
 
 module.exports = Object.freeze({
   setup: function() {
-
+    db.run('create table buy (' +
+        'id integer primary key autoincrement, ' +
+        'name text, ' +
+        'price real');
+    db.run('create table sell (' +
+        'id integer primary key autoincrement, ' +
+        'name text, ' +
+        'price real');
   },
   seed: function() {
 
