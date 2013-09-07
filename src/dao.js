60e1470a2fc655b5f9b71adca300586c341316b7
William B. Wettersten <wbw20@case.edu>
2013-09-06 21:40:08 -0400

    eliminate timing hazards the stupid simple way

--- src/dao.js
+++ src/dao.js
@@ -3,17 +3,17 @@ var db = new sqlite3.Database(':memory:');
 
 module.exports = Object.freeze({
   setup: function() {
-    db.run('create table buy (' +
+    db.run(
+      'create table buy (' +
         'id integer primary key autoincrement, ' +
         'name text, ' +
-        'price real);');
-    db.run('create table sell (' +
+        'price real);  ' +
+      'create table sell (' +
         'id integer primary key autoincrement, ' +
         'name text, ' +
-        'price real);');
-  },
-  seed: function() {
-
+        'price real);  ' +
+      'insert into buy values (1, \'Will\', 3.50);  ' +
+      'insert into sell values (1, \'Charles\', 4.25);');
   },
   buys: function(options) {
 
